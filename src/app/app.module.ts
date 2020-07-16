0d92d49a39987d6007c47cf1064cc6d73fac4f01
Fabian Ehehalt <>
2020-07-16 22:40:04 +0200

    style: 💄 adding bootstrap
    Adding Bootsrap and testing on header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-
-import { AppComponent } from './app.component';
-import { LandingpageComponent } from './landingpage/landingpage.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './landingpage/navbar/navbar.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MainComponent } from './landingpage/main/main.component';
-import { FooterComponent } from './landingpage/footer/footer.component';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ScullyLibModule } from '@scullyio/ng-lib';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './landingpage/footer/footer.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { MainComponent } from './landingpage/main/main.component';
+import { NavbarComponent } from './landingpage/navbar/navbar.component';
 
 @NgModule({
   declarations: [AppComponent, LandingpageComponent, NavbarComponent, MainComponent, FooterComponent],
