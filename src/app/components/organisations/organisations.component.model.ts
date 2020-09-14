1424f4bd7cb4f21cfd343acb6e01b24431746b00
Fabian Ehehalt <>
2020-09-14 17:26:14 +0200

    fix: 🐛 edited text

--- src/app/components/organisations/organisations.component.model.ts
+++ src/app/components/organisations/organisations.component.model.ts
@@ -11,10 +11,10 @@ export class OrganisationComponentModel {
     this.organisationheadline = 'Eure Organisation möchte dabei sein?';
     this.ehrenamt = [
       'Wir bringen frischen Wind',
-      'Dem Ehrenamt fehlt es zunehmend an tragenden Schultern: Wir wollen das ändern.' +
-        'Die Plattform erleichtert Motivierten den Einstieg, nimmt Druck und Ängste, erweitert die möglichkeiten, verpasst ' +
+      'Dem Ehrenamt fehlt es zunehmend an tragenden Schultern. Wir wollen das ändern. ' +
+        'Die Plattform erleichtert Motivierten den Einstieg, nimmt Druck und Ängste, erweitert die Möglichkeiten, verpasst ' +
         'sozialem Engagement ein frischeres Image und steigert die Wertschätzung. ' +
-        'Ihr könnt durch impact also mit vielen neuen MitstreiterInnen rechnen - speziell die Jüngeren möchten wir durch unser Konzept ' +
+        'Ihr könnt durch impact mit vielen neuen MitstreiterInnen rechnen - speziell die Jüngeren möchten wir durch unser Konzept ' +
         'erreichen und von unserer Vision überzeugen. '
     ];
     this.knoten = [
@@ -27,7 +27,7 @@ export class OrganisationComponentModel {
     this.ergaenzen = [
       'Wir ergänzen uns',
       'Unser Team hat das notwendige Know-How, das ambitionierte Projekt umzusetzen. ' +
-        'Um Euch bestmöglich zu unterstützen, brauchen wir jedoch Euch mit Eurer langjährigen Praxiserfahrung im Ehrenamt als Input. ' +
+        'Um Euch bestmöglich zu unterstützen, brauchen wir jedoch Eure langjährige Praxiserfahrung im Ehrenamt als Input. ' +
         'Wir verstehen uns gewissermaßen als "Dienstleister" für Euren so wertvollen Einsatz für die Gesellschaft, ' +
         'weshalb wir uns einen engen Austausch und langfristige Partnerschaften wünschen.'
     ];
