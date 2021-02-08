5508cb694ad909519b0c7d34cc03e6c0b28e68d3
Fabian Ehehalt <fabian@AirFabian.fritz.box>
2021-02-08 19:18:58 +0100

    feat: 🎸 Some animations

--- src/app/components/landingpage/landingpage.component.desktop.ts
+++ src/app/components/landingpage/landingpage.component.desktop.ts
@@ -4,6 +4,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { PageScrollService } from 'ngx-page-scroll-core';
+import { Typewriter } from 'typewriter-effect/dist/core';
 import { ApplicationStateService } from '../../services/application-state.service';
 import { InstagramFeedService } from '../../services/instagram-feed.service';
 import { LandingpageComponent } from './landingpage.component';
@@ -16,7 +17,6 @@ import { LandingpageComponent } from './landingpage.component';
 })
 export class LandingpageDesktopComponent extends LandingpageComponent {
   public instaFeed = [];
-
   constructor(
     router: Router,
     sanitizer: DomSanitizer,
@@ -28,5 +28,22 @@ export class LandingpageDesktopComponent extends LandingpageComponent {
   ) {
     super(router, sanitizer, applicationStateService, snackBar);
     this.instaFeed = this.instagramFeedService.getInstaFeed();
+
+    const app = document.getElementById('app');
+
+    const typewriter = new Typewriter(app, {
+      loop: true,
+      delay: 75
+    });
+
+    typewriter
+      .pauseFor(2500)
+      .typeString("What's your impact")
+      .pauseFor(300)
+      .deleteChars(10)
+      .typeString('<strong>JS</strong> plugin for a cool typewriter effect and ')
+      .typeString('<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>')
+      .pauseFor(1000)
+      .start();
   }
 }
