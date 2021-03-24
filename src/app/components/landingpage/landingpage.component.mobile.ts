0f5f8eff019cffd7508496ae17d066c49f2e8b44
Fabian Ehehalt <fabian@AirFabian.fritz.box>
2021-03-24 20:43:59 +0100

    fix: 🐛 reload onResize

--- src/app/components/landingpage/landingpage.component.mobile.ts
+++ src/app/components/landingpage/landingpage.component.mobile.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, ViewEncapsulation } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
@@ -19,5 +19,12 @@ export class LandingpageMobileComponent extends LandingpageComponent {
   constructor(router: Router, sanitizer: DomSanitizer, applicationStateService: ApplicationStateService, snackBar: MatSnackBar, private instagramFeedService: InstagramFeedService) {
     super(router, sanitizer, applicationStateService, snackBar);
     this.instaFeed = this.instagramFeedService.getInstaFeed();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(): void {
+    if (window.innerWidth > 768) {
+      window.location.reload();
+    }
   }
 }
--- src/app/components/sponsoring/sponsoring.component.mobile.ts
+++ src/app/components/sponsoring/sponsoring.component.mobile.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { ApplicationStateService } from '../../services/application-state.service';
@@ -12,5 +12,12 @@ import { SponsoringComponent } from './sponsoring.component';
 export class SponsoringMobileComponent extends SponsoringComponent {
   constructor(router: Router, sanitizer: DomSanitizer, applicationStateService: ApplicationStateService) {
     super(router, sanitizer, applicationStateService);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(): void {
+    if (window.innerWidth > 768) {
+      window.location.reload();
+    }
   }
 }
