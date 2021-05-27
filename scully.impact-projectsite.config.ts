f9308250b28fbb5cbe6d0c6fc3a072182b435734
fabianehehalt <63912322+fabianehehalt@users.noreply.github.com>
2021-05-27 18:26:58 +0200

    Fixes

--- scully.impact-projectsite.config.ts
+++ scully.impact-projectsite.config.ts
@@ -1,8 +1,3 @@
 import { ScullyConfig } from '@scullyio/scully';
-export const config: ScullyConfig = {
-  projectRoot: 'src/app',
-  projectName: 'impact-projectsite',
-  outDir: 'dist/scully',
-  routes: {
-  }
+export const config: ScullyConfig = { projectRoot: 'src/app', projectName: 'impact-projectsite', outDir: 'dist/scully', routes: {}
 };
